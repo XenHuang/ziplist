860a4171f520534f62ec6c5828b6a17185e4c3c8
Xen <zhenfeng@hawaii.edu>
2016-09-18 15:31:09 -1000

    finished Ziplist

---
+++ ziplist.js
@@ -0,0 +1,23 @@
+/**
+ * E27
+ * Created by X on 2016/9/18.
+ */
+
+const testList1 = ['a', 'b', 'c'];
+const testList2 = [1, 2 ,3];
+
+function zipList (list1, list2) {
+  const ziplist = [];
+  for (let i = 0; i < list1.length; i++){
+    ziplist.push(list1[i], list2[i]);
+  }
+  return ziplist;
+}
+
+console.log(zipList(testList1, testList2));
+
+function zipListTheSimpleWay (list1, list2) {
+  return _.flatten(_.zip(list1, list2));
+}
+
+console.log(zipListTheSimpleWay(testList1, testList2));
